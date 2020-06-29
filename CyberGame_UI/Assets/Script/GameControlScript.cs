387512528fa035d87106f18a02bd7bd34ebba296
LAMZA <lamzahk@yahoo.com.hk>
2020-06-29 13:41:25 +0900

    新しいCYBER GAME UIを追加。

---
+++ CyberGame_UI/Assets/Script/GameControlScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour
+{
+    [SerializeField] GameObject Fade_start;
+    GameObject MainCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCanvas = GameObject.Find("MainMenu_Canvas");
+        if(MainCanvas == null)
+        {
+            MainCanvas = GameObject.Find("Shop_Canvas");
+
+        }
+        GameObject obj = (GameObject)Instantiate(Fade_start);
+        obj.transform.SetParent(MainCanvas.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) Quit();
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+   
+      
+   
+}
---
+++ CyberGame_UI/Assets/Script/SceneMoveScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMoveScript : MonoBehaviour
+{
+    GameObject MainCanvas;
+    //[SerializeField] GameObject Fade_start;
+    [SerializeField] GameObject Fade_end;
+    string btn_name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCanvas = GameObject.Find("MainMenu_Canvas");
+        if (MainCanvas == null)
+        {
+            MainCanvas = GameObject.Find("Shop_Canvas");
+
+        }
+
+        btn_name = this.gameObject.name;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneMoveMethod()
+    {
+
+        StartCoroutine("fade_method");
+
+    }
+    private IEnumerator fade_method()
+    {
+       
+
+        GameObject obj = Instantiate(Fade_end, this.transform.position, Quaternion.identity) as GameObject;
+        obj.transform.parent = MainCanvas.transform;
+        yield return new WaitForSeconds(0.25f);
+
+        switch (btn_name)
+        {
+            case "EquipmentBtn":
+
+               
+                //UnityEngine.SceneManagement.SceneManager.LoadScene();
+                Debug.Log("Equipment");
+
+                break;
+
+            case "UnitBtn":
+
+               
+                //UnityEngine.SceneManagement.SceneManager.LoadScene();
+
+                Debug.Log("Unit");
+
+                break;
+
+            case "ShopBtn":
+                
+                UnityEngine.SceneManagement.SceneManager.LoadScene("CyberGame_Shop");
+                Debug.Log("Shop");
+
+                break;
+
+            case "BattleBtn":
+                
+                //UnityEngine.SceneManagement.SceneManager.LoadScene();
+                Debug.Log("Battle");
+
+                break;
+
+            default:
+
+                Debug.Log("Default");
+
+                break;
+        }
+    }
+}
